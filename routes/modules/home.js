636a32d2ac6090470b554e74f37c03c26cc9acfe
JIN-SKILL <huangjinhu.1995@gmail.com>
2021-03-18 17:02:23 +0800

    feat: add doneTime function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,26 +1,43 @@
 const express = require('express')
 const router = express.Router()
 
-const now = new Date()
+router.use((req, res, next) => {
+  const reqTime = +new Date() //正號(+)是強制轉為時間戳記
+  res.on('finish', () => {
+    const resTime = new Date()
+    const doneTime = +new Date() - reqTime
+    const [YYYY, MM, DD, hh, mm, ss] = [
+      resTime.getFullYear().toString().padStart(2, '0'),
+      (resTime.getMonth() + 1).toString().padStart(2, '0'),
+      resTime.getDate().toString().padStart(2, '0'),
+      resTime.getHours().toString().padStart(2, '0'),
+      resTime.getMinutes().toString().padStart(2, '0'),
+      resTime.getSeconds().toString().padStart(2, '0')
+    ]
+    console.log(`${YYYY}/${MM}/${DD} ${hh}:${mm}:${ss} | total time: ${doneTime}ms | ${req.method} from ${req.path}`)
+  })
+  next()
+})
 
 router.get('/', (req, res) => {
-  console.log(`${now.getFullYear()}/${now.getMonth()}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} | GET from /`)
-  res.send('列出全部 Todo')
+  res.send(`
+  <form action="/" method="POST">
+    <p>列出全部 Todo</p>
+    <button type="submit">新增</button>
+  </form>
+  `)
 })
 
 router.get('/new', (req, res) => {
-  console.log(`${now.getFullYear()}/${now.getMonth()}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} | GET from /new`)
   res.send('新增 Todo 頁面')
 })
 
 router.get('/:id', (req, res) => {
-  console.log(`${now.getFullYear()}/${now.getMonth()}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} | GET from /:id`)
   res.send('顯示一筆 Todo')
 })
 
 router.post('/', (req, res) => {
-  console.log(`${now.getFullYear()}/${now.getMonth()}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} | POST from /`)
-  res.send('新增一筆  Todo')
+  res.send('新增一筆 Todo')
 })
 
 module.exports = router
